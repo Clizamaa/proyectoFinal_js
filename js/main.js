c5b1e1d03225ab06477e7563010a0012f476e8f5
Christian <cfla86@gmail.com>
2024-03-06 17:00:57 -0300

    consumiendo api

--- js/main.js
+++ js/main.js
@@ -1,2 +1,59 @@
 
-  
+  const url = 'https://dummyjson.com/products'
+
+  const getProducts = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+
+    const products = data.products.map(product => {
+        return `
+            <div class="product">
+            <div class="block rounded-lg bg-white w-72 mt-4" id="products">
+                <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
+                    <img class="rounded-lg  sm:m-h-64 md:h-64 w-full carts" src="${product.thumbnail}" alt="${product.brand}" />
+                    <a href="#!">
+                        <div
+                            class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="p-2">
+                    <div class="flex justify-between">
+                        <h5 class="mb-2 text-sm font-bold leading-tight text-neutral-800">
+                            ${product.title}
+                        </h5>
+                        <h5 class="mb-2 text-sm font-bold leading-tight text-neutral-800 ">
+                            ${product.rating} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                class="w-4 h-4 ml-1">
+                                <path fill-rule="evenodd"
+                                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </h5>
+                    </div>
+                    <p class="mb-1 text-sm text-neutral-600">
+                        ${product.description}
+                    </p>
+        
+                    <p class="mb-4 text-base text-neutral-600">
+                        $${product.price}
+                    </p>
+                    <h5 class="mb-2 text-sm font-bold leading-tight text-neutral-800">
+                        
+                    </h5>
+                </div>
+              
+            </div>
+            
+            </div>
+        `
+        }).join('')
+        console.log(products)
+        document.getElementById('products').innerHTML = products
+
+
+  }
+
+    getProducts()
